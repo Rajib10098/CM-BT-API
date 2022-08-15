3216d3ae68c95953a13cf04838a739120aac4d16
Rajib10098 <82735278+Rajib10098@users.noreply.github.com>
2022-08-15 13:04:16 +0530

    Create TestOne.js

---
+++ TestOne.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", function () {
+
+            fetch('https://www.googleapis.com/blogger/v3/blogs/6002919485693520244/posts/search?q=thefatrat&key=AIzaSyDM7PgWDnsmjmusZ1K7sfoeBQCcOSHkLmA').then((response) => response.json()).then((data) => {
+
+                let displaySeeMore = "";
+                for (let i = 0; i < data.items.length; i++) {
+                    let findImage = data.items[i].content
+                    let resizingTheImage = new URL(findImage.match(/\bhttps?:\/\/blogger.googleusercontent.com\/\S+/gi)).href.replace("s1600", "s150")
+
+
+                   // console.log(data.items)
+                    console.log(data)
+
+
+                    displaySeeMore += `  
+        <div class="SeeMoreContainer">
+        
+        <a style="text-decoration: none;" href="${data.items[i].url}">
+            <div class="SeeMoreImageContainer"><p class="smLoading">Loading...</p>
+            <img class="SeeMoreImage" src="${resizingTheImage}" alt="" loading="lazy">
+        </div>
+        </a>
+
+        <div class="SeeMoretitleContainer">
+            <a style="text-decoration: none;" href="${data.items[i].url}"><p class="SeeMoreSongTitle">${data.items[i].title}</p></a>
+        </div>
+
+      </div>`;
+
+
+                    document.getElementById("See-more-container").innerHTML = displaySeeMore;
+
+                }
+
+
+
+
+            })
+
+
+        })
